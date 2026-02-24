76519cbba1c8b73d9911a0919ae515bd417ab281
hgasoft <49619916+hgasoft@users.noreply.github.com>
2026-02-24 14:43:08 +0300

    Fix method signature and update method call

--- src/Table/BaseAudioVideoPlayerColumn.php
+++ src/Table/BaseAudioVideoPlayerColumn.php
@@ -2,15 +2,15 @@
 
 namespace AymanAlhattami\FilamentAudioVideoPlayer\Table;
 
-use Filament\Tables\Columns\Column;
 use AymanAlhattami\FilamentAudioVideoPlayer\WithAudioVideoPlayer;
+use Filament\Tables\Columns\Column;
 
-abstract class BaseAudioVideoPlayerColumn  extends Column
+abstract class BaseAudioVideoPlayerColumn extends Column
 {
     use WithAudioVideoPlayer;
 
-    public static function make(string $name): static
+    public static function make(?string $name = null): static
     {
-        return parent::make($name)->disableClick();
+        return parent::make($name)->disabledClick();
     }
 }
